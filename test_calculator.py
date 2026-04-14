f58ab604c0b8fb19df1d01a0ce79a5cb9c8b91f1
Edison Lara-Bojay <edisonlara1212@gmail.com>
2026-04-13 22:48:06 -0400

    Wrote test cases

--- test_calculator.py
+++ test_calculator.py
@@ -11,12 +11,16 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self): # 3 assertions
+        assert multiply(1, 1000) == 1000
+        assert multiply(-0.5, 20) == -10
+        assert multiply(-10, -10) == 100
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self): # 3 assertions
+        assert div(100, 1) == 1
+        assert div(10, -0.5) == -20
+        assert div(-10, -20) == 0.5
+     ##########################
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -34,21 +38,24 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self): # 1 assertion
+        with self.assertRaises(ValueError):
+              log(-10, 1)
+              
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self): # 3 assertions
+        assert hypotenuse(3, 4) == 5
+        assert hypotenuse(6, 8) == 10
+        assert hypotenuse(15, 8) == 17
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
+    def test_sqrt(self): # 3 assertions
+        with self.assertRaises(ValueError):
+              square_root(-10)
+
+        assert square_root(100) == 10
+        assert square_root(225) == 15
+        assert square_root(25) == 5
+        
     ##########################
 
 # Do not touch this
